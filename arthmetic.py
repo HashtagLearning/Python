6b7d79eab587ed180ac2aba8e9981c502effcd99
sheik.ahamed <sheik.ahamed@indusnet.co.in>
2021-06-11 19:55:15 +0530

    added arth.py and condition.py files to project

---
+++ arthmetic.py
@@ -0,0 +1,30 @@
+# +, -, /,*
+
+# add
+add = 5+5
+a1 = 5
+a2 = 5
+a3 = a1 + a2
+print(add, a3)
+
+#sub
+sub = 4-4
+b1 = 4
+b2 = 4
+b3 = b1 - b2
+print(sub, b3)
+
+# saquib mul
+
+#mul 
+mul = 3*4
+c1 = 3
+c2 = 4
+c3 = c1*c2
+print (mul, c3)
+
+#div
+print(4/2)
+
+#power
+print(2**200)
